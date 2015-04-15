af1392733fabf619652a597845f065c7ca26eec4
lseguradolacera <lseguradolancera@gmail.com>
2015-04-15 12:43:45 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,37 +1,43 @@
 <html>
 <head> <title>blog</title>
+<link rel="styesheet" type="text/css" href="layout.css">
 <style>
 body {
 	font-family: "myriad pro", arial, sans-serif;
 	margin:auto;
 }
 #container{
-	margin-left:550px;
+	margin-left:500px;
 	margin-top:130px;
 	width:315px;
-	padding:8px;
-	background-color:#cccccc;
-	border-radius:8px;
+	
 	}
 #head{
 	background-color:yellowgreen;
 	}
+#message_entry{
+	width:315px;
+	background-color:#cccccc;
+	border-radius:8px;
+	padding:15px;
+	height:250px;
+	
+	}
 #inputs{
 	height:35px;
-	width:305px;
+	width:310px;
 	border-radius:5px;
 	border:1px solid;
-	border-color:yellowgreen;
+	border-color:white;
 	padding:5px;
 	}
 #textarea{
 	height:200px;
-	width:305px;
+	width:310px;
 	border-radius:5px;
 	border:1px solid;
-	border-color:yellowgreen;
+	border-color:white;
 	padding:5px;
-	
 	}
 #submit{
 	height:35px;
@@ -39,41 +45,54 @@ body {
 	color:white;
 	border-radius:5px;
 	border:1px solid;
-	border-color:green;
-	background-color:yellowgreen;
+	border-color:#99ff33;
+	background-color:#99ff33;
 	}	
+#view_all{
+	float:right;
+	}
 	a:link {
     text-decoration: none;
-		color:black;
-}
+		color:#99ff33;
+}l
 </style>
 </head>
 
 <body>
 <div id="container">
-		<form  method="POST">
+		<div id="view_all"><a href="view.php"><b><font size="4px">VIEW ALL</font></b></a></div>
+		<br/>
+		<br/>
+	<form  method="POST">
+		<div id="message_entry">
+		
 			<table>
-				<tr><th>Pls fill in the asked information.</th></tr>
-				<tr><td>Name</tr></td>
 				<tr><td><input id="inputs" type="text" name="name" placeholder="Name" required/></td></tr>
-				<tr><td>Message:</td></tr>
-				<tr><td><textarea  id="textarea" name="msg" placeholder="Your Message"></textarea></td></tr>
+				<tr><td><textarea  id="textarea" name="message" placeholder="Your Message"></textarea></td></tr>
 			</table>
+			
+		</div>
+		<br/>
+		<div id="send">
 			<table>
 				<tr><td><input id="submit" type="submit" value="Send" name="submit"></td>
-				<td><a href="view.php"><b><font size="4px">VIEW ALL</font></b></a></td>
 				</tr>
 			</table>
+		</div>
 		</form>
 		
 		<?php
 			if (isset($_POST['submit']))
 				{	  
-					include 'function.php';
+					include 'connect.php';
+					$name=$_POST['name'] ;
+					$message= $_POST['message'] ;					
+					mysql_query("INSERT INTO message (name,message) 
+					VALUES ('$name','$message')");
 				}
 		?>
-		
 </div>
 
 </body>
 </html>
+
