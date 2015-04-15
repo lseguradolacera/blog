1a37377ff5ccdd5a8c081edd46211c7e1eee5497
Arlyn Sarita <a.sarita@gmail.com>
2015-04-15 00:46:01 -0700

    Update view.php

--- view.php
+++ view.php
@@ -13,22 +13,58 @@ body{
 		font-family: calibri;
 		background: whitesmoke;
 	}
+	#comments{
+		float:left;
+		width:310px;
+		padding:10px;
+		}
+	#message{
+		width:290px;
+		padding:15px;
+		background-color:#529bfd;
+		border-radius:9px;
+		color:white;
+		}
+	#name{
+		width:290px;
+		height:30px;
+		padding:3px;
+		background-color:#333333;
+		border-radius:5px;
+		}
+		a:link {
+    text-decoration: none;
+		color:#cccccc;
+}
+	
 </style>
 
 <center><h1>BLOG</h1>
+<hr color="grey">
 <?php
 	$con = mysqli_connect("localhost", "root", "", "blog");
-	$result = mysqli_query($con, "SELECT * FROM message");
+	$result = mysqli_query($con, "SELECT * FROM message order by number");
 	
 	echo "<table border='0'>";
 	while($row = mysqli_fetch_array($result))
-	{
-		echo "<tr><td colspan='1' style='background: grey;'><b>" . $row['name'] . ":</b></td></tr>";
-		echo "<tr><td colspan='2'>--- " . $row['msg'] . "</td></tr>";
+	{	
+		echo"<tr>";
+		echo "<div id='comments'>";
+			echo"<div id='message'>";
+				echo $row['msg']."<br/>";
+				echo"<br />";
+				echo "<div id='name'>";
+				echo "<b>". $row['name'] ."</b>";
+				echo "<i>"." - ".date('jS \of F Y')."</i>";
+				echo "</ div>";
+			echo"</div>";
+		echo "</div>";
+		echo"</tr>";
 	}
 	echo "</table></center>";
 	mysqli_close($con)
 ?>
+
 <footer><center><a href='index.php'>Home</a></center></footer>
 </body>
 </html>
