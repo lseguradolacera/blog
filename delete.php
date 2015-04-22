f7e2a9af4aa4ed1c6dc0736fb79420c03a1ff876
yokeks <kevinrgirasol@gmail.com>
2015-04-22 12:04:56 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,29 +1,33 @@
 <?php 
+
 include ('DAL.php');
+include ('header.php');
+session_start();
+
 $connection = new DAL();
 $table = "message";
-		if(isset($_GET['delete_id']))
-		{
-			$id=$_GET['delete_id'];
-			$res=$connection->DeleteMessage($table,$id);
-			if($res)
-			{
-				?>
-				<script>
-				alert('Record Deleted ...')
-						window.location='view.php'
-						</script>
-				<?php
-			}
-			else
-			{
-				?>
-				<script>
-				alert('Record cannot be Deleted !!!')
-						window.location='view.php'
-						</script>
-				<?php
-			}
-		}
+
 	
+			$username = $_SESSION['userid'];
+			$id=$_GET['delete_id'];
+			$userid=$_GET['user_id'];
+			
+			echo $username;
+			echo $userid;
+			echo $id;
+			$connection->DeleteMessage($table,$id,$userid);
+			
+			$res= $connection->checkDelete($userid);
+			
+			
+			if(!$res) 
+			{
+				?>
+				<script>
+				
+				alert('Ok!!!')
+						window.location='view.php'
+						</script>
+				<?php
+			}	
 	?>
