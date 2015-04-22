a28098296a7e7350054a908e6f27da10ee5098d4
yokeks <kevinrgirasol@gmail.com>
2015-04-22 12:04:14 +0800

    Update DAL.php

--- DAL.php
+++ DAL.php
@@ -9,29 +9,38 @@ class DAL{
 		//initialize the DB connection here, assign to $connection
 	}
 	
-	public function AddMessage($name,$message){
+	public function AddMessage($name,$message,$userid,$username){
 		
-		$res = mysqli_query($this->Connection,"INSERT INTO message(name,msg) VALUES('$name','$message')");
+		$res = mysqli_query($this->Connection,"INSERT INTO message(name,msg,userid,username) VALUES('$name','$message',$userid,'$username')");
 		
 	}	
 	
-	public function ViewMessage(){
+	public function ViewMessage($userid){
 		
-		$res = mysqli_query($this->Connection,"SELECT * FROM message");
+		$res = mysqli_query($this->Connection,"SELECT * FROM message ORDER BY userid = $userid");
 		return $res;
 		
 	}	
 	
-	public function DeleteMessage($table,$id){
+	public function DeleteMessage($table,$id,$userid){
 		
-		$res = mysqli_query($this->Connection,"DELETE FROM message WHERE number=".$id);
-		
+		$res = mysqli_query($this->Connection,"DELETE FROM message WHERE number= '$id' AND userid = '$userid'");
 	}	
-	
+
 	public function EditMessage($table,$id,$name,$message){
 		
 		$res = mysqli_query($this->Connection,"UPDATE message SET name='$name', msg='$message' WHERE number=".$id);
 		return $res;
+		//$this->Connection
+	}
+	public function CheckDelete($userid){
+		$res = mysqli_query($this->Connection,"SELECT  FROM message, users WHERE userid ='$userid'");
+		return $res;
+		echo $res;
+	}public function CheckEdit($userid){
+		$res = mysqli_query($this->Connection,"SELECT  FROM message, users WHERE userid ='$userid'");
+		return $res;
+		echo $res;
 	}
 	
 	
