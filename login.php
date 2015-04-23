5029a9140525f3d324320717ae55e90471704796
yokeks <kevinrgirasol@gmail.com>
2015-04-23 09:02:36 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,3 @@
-<?php
-include('loginfunction.php'); 
-
-if(isset($_SESSION['login_user'])){
-header("location: homepage.php");
-}
-
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -19,11 +11,29 @@ header("location: homepage.php");
 <label>UserName :</label>
 <input id="name" name="username" placeholder="Username" type="text">
 <label>Password :</label>
-<input id="password" name="password" placeholder="**********" type="password">
-<input name="submit" type="submit" value=" Login ">
-<span><?php echo $error; ?></span>
+<input id="password" name="password" placeholder="Password" type="password">
+<input name='submit' type="submit" value='submit'>
 </form>
 </div>
 </div>
 </body>
 </html>
+
+<?php  
+error_reporting(1);
+    include_once('dbLoginFunction.php');  
+       
+    $funObj = new dbFunction();  
+    if($_POST['submit']){
+        $username = $_POST['username'];  
+        $password = $_POST['password']; 
+        $user = $funObj->Login($username, $password);
+		if ($user) {  
+								// Login Success  
+							 header("location:index.php?user=$username");  
+						} else {  
+								// Failed  
+								echo "<script>alert('Username / Password Not Match')</script>";  
+						}  
+				}  
+?>  
