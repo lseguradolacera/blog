0f9829d11b2774535b3e8a56a568c7797fc9dc2f
codeHELLOWORLD <hcabanoglancera@gmail.com>
2015-04-22 20:07:47 -0700

    Create indexhend.php

---
+++ indexhend.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'core/init.php';
+
+if(Session::exists('home')){
+	echo '<p>' . Session::flash('home') . '</p>';	
+}
+
+$user = new User(); 
+if($user->isloggedIn()){
+	echo 'Logged in'; 
+
+?>
+	<p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?></a>"><?php echo escape($user->data()->username); ?></a>! </p>
+
+	<ul>
+		<li><a href="logout.php">Log out </a></li>
+		<li><a href="update.php">Update details </a></li>
+		<li><a href="changepassword.php">Change Password </a></li>
+	</ul>
+
+<?php 
+if($user->hasPermission('administrator')){
+	echo '<p>You are an administator!</p>';
+}
+
+} else {
+	echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
+}
